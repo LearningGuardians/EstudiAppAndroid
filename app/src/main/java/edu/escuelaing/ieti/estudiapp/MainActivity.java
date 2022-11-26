ca68aadbadd304322ca02aff8f16e3626ddcfd83
diego gonzalez <diego.gonzalez-g@mail.escuelaing.edu.co>
2022-11-26 01:19:00 -0500

    Dependencia para splashscreen y comienzo de conexion con botones

--- app/src/main/java/edu/escuelaing/ieti/estudiapp/MainActivity.java
+++ app/src/main/java/edu/escuelaing/ieti/estudiapp/MainActivity.java
@@ -1,13 +1,31 @@
 package edu.escuelaing.ieti.estudiapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        nextPage();
+    }
+
+    private void nextPage(){
+        Button nextButton = (Button) findViewById(R.id.PlanEstudioButton);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,PlanEstudio_Create.class));
+            }
+        });
     }
 }
+
---
+++ app/src/main/java/edu/escuelaing/ieti/estudiapp/PlanEstudio_Create.java
@@ -0,0 +1,14 @@
+package edu.escuelaing.ieti.estudiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlanEstudio_Create extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plan_estudio_create);
+    }
+}
