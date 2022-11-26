a34e041f061a5af72348e3740a8e2f362ef096ee
diego gonzalez <diego.gonzalez-g@mail.escuelaing.edu.co>
2022-11-26 03:44:11 -0500

    Funcion de hora realizada para plan de estudio

--- app/src/main/java/edu/escuelaing/ieti/estudiapp/PlanEstudio_Create.java
+++ app/src/main/java/edu/escuelaing/ieti/estudiapp/PlanEstudio_Create.java
@@ -2,13 +2,42 @@ package edu.escuelaing.ieti.estudiapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+
+import java.util.Locale;
 
 public class PlanEstudio_Create extends AppCompatActivity {
+
+    Button timeButton;
+    int hour,minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan_estudio_create);
+        timeButton = findViewById(R.id.hourButton);
+    }
+
+    /**
+     * Funcion genereada para escuchar la hora que escogio el estudiante en la cual va a llegar a la casa.
+     * @param view
+     */
+    public void timePick(View view){
+        TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                hour = selectedHour;
+                minute = selectedMinute;
+                timeButton.setText(String.format(Locale.getDefault(),"%02d:%02d",hour,minute));
+            }
+        };
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,timeSetListener,hour,minute,true);
+        timePickerDialog.setTitle("Seleccionar Hora");
+        timePickerDialog.show();
     }
 }
