c597f7aed1306abdbd2e9d23a72b8aee88ce913a
diego gonzalez <diego.gonzalez-g@mail.escuelaing.edu.co>
2022-11-26 03:06:00 -0500

    Terminacion de carrusel plan de estudios

--- app/src/main/java/edu/escuelaing/ieti/estudiapp/PlanOperativo.java
+++ app/src/main/java/edu/escuelaing/ieti/estudiapp/PlanOperativo.java
@@ -7,23 +7,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class PlanOperativo extends AppCompatActivity {
 
+    /**
+     * Funcion main de la actividad PlanOperativo.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.plan_operativo);
         nextPage();
     }
 
+    /**
+     * Funcion generada para redirigir hacia la actividad de creacion de plan de estudios al cliente.
+     * Esto mediante un listener, el cual revisa si se ha oprimido el botón.
+     */
     private void nextPage(){
         Button nextButton = (Button) findViewById(R.id.PlanEstudioButton);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,PlanEstudio_Create.class));
+                startActivity(new Intent(PlanOperativo.this,PlanEstudio_Create.class));
             }
         });
     }
